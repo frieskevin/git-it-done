50111e6090b695682524b4d470740c15be6a606f
alotofsnakes <mrfries@gmail.com>
2022-08-12 17:49:28 -0700

    finished up list-issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,6 @@
 var issueContainerEl = document.querySelector('#issues-container');
+var limitWarningEl = document.querySelector('#limit-warning');
+
 
 var getRepoIssues = function(repo) {
     var apiUrl = 'https://api.github.com/repos/' + repo + '/issues?direction=asc';
@@ -6,11 +8,15 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert('There was a problem with your request!');
         }
     });
+
     console.log(repo);
 };
 
@@ -36,4 +42,15 @@ var displayIssues = function(issues) {
         issueContainerEl.appendChild(issueEl);
     }
 };
+
+var displayWarning = function(repo) {
+    limitWarningEl.textContent = 'To see more than 30 issues, visit ';
+    var linkEl = document.createElement('a');
+    linkEl.textContent = 'GitHub.com'
+    linkEl.setAttribute('href', 'https://github.com/' + repo + '/issues');
+    linkEl.setAttribute('target', '_blank');
+    limitWarningEl.appendChild(linkEl);
+
+};
+
 getRepoIssues('facebook/react');
