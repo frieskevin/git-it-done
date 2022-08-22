8caa2b2a30e9b98dcbfa16e09c62d81677ad3eda
alotofsnakes <mrfries@gmail.com>
2022-08-21 20:33:20 -0700

    added a languge search function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector('#user-form');
 var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchterm = document.querySelector('#repo-search-term');
+var languageButtonsEl = document.querySelector('#language-buttons');
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -13,6 +14,19 @@ var formSubmitHandler = function(event) {
     } else {
         alert('Please enter a GitHub username');
     }
+};
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
 };
 
 var getUserRepos = function(user) {
@@ -59,4 +73,15 @@ var displayRepos = function(repos, searchTerm) {
     }
 };
 
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute('data-language');
+    if (language) {
+        getFeaturedRepos(language);
+        repoContainerEl.textContent = '';
+    }
+    console.log(language)
+}
+
 userFormEl.addEventListener('submit', formSubmitHandler);
+
+languageButtonsEl.addEventListener('click', buttonClickHandler);
